fd572c431f13d9e47972ecf1a730c4741cf5bcc0
Eugene Belkovich <eugene.belkovich@gmail.com>
2018-10-01 01:25:30 +0300

    map updated

--- src/Map.js
+++ src/Map.js
@@ -2,23 +2,24 @@ import React from 'react'
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 
 class Map extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      lat: 50,
-      lng: 10,
-      zoom: 6
-    }
-  }
-
   render() {
-    const position = [this.state.lat, this.state.lng];
     return (
-      <LeafletMap center={position} zoom={this.state.zoom}>
+      <LeafletMap
+        center={[50, 10]}
+        zoom={6}
+        maxZoom={10}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        <Marker position={position}>
+        <Marker position={[50, 10]}>
           <Popup>
             Popup for any custom information.
           </Popup>
